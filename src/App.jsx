ed10b859211991f5524bd5944c44b6c3dfb0a4cd
SamX23 <skalexsong@gmail.com>
2022-08-15 13:05:08 +0700

    Update : do checklist algorithm

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,71 @@ function App() {
 
   const onsetCurrentValue = (e) => {
     const currentInput = e.target.value;
+    const operator = ["*", "+", "-", "/"];
     const operatorAttribute = e.target.dataset.attr;
     const ifValueEmpty = currentValue.length == 0;
     const ifCurrentInputIsNotZero = currentInput != 0;
+    const lastInput = currentValue[currentValue.length - 1];
 
+    const inputIsOperator = (input) => operator.includes(input);
+
+    const isIsogram = (str) => {
+      return /(.).*\1/.test(str);
+    };
+
+    let x = currentValue.some(function (v, i, a) {
+      if (i === ".") {
+        return a.lastIndexOf(v) != i;
+      }
+    });
+
+    // Prevent zero and operator on first input
+    const firstCalculation = () =>
+      ifCurrentInputIsNotZero &&
+      !inputIsOperator(currentInput) &&
+      setCurrentValue([...currentValue, currentInput]);
+
+    // First calculation
     if (currentResult == 0) {
+      // On first input
       if (ifValueEmpty) {
-        if (ifCurrentInputIsNotZero) {
-          if (operatorAttribute == undefined) {
+        firstCalculation();
+      } else {
+        // On the second and rest input
+        // If input is operator
+        if (inputIsOperator(currentInput)) {
+          // If last input was operator
+          if (inputIsOperator(lastInput)) {
+            const newArr = currentValue;
+
+            // Add minus number if current input "-"
+            if (currentInput == "-") {
+              newArr.push(currentInput);
+
+              // If last input was "-" operator, replace all operator (second and third added operator)
+            } else if (lastInput == "-") {
+              newArr.splice(newArr.length - 2, 2);
+              newArr.push(currentInput);
+            } else {
+              newArr.push(currentInput);
+            }
+
+            setCurrentValue(newArr);
+          } else {
             setCurrentValue([...currentValue, currentInput]);
           }
+        } else {
+          setCurrentValue([...currentValue, currentInput]);
         }
-      } else {
-        setCurrentValue([...currentValue, currentInput]);
       }
     } else {
+      // Additional calculation
       if (operatorAttribute) {
         setCurrentValue([...currentValue, currentResult, currentInput]);
       } else {
+        // If no calculation, reset to calculate new result
         setCurrentResult(0);
-        setCurrentValue([...currentValue, currentInput]);
+        firstCalculation();
       }
     }
   };
@@ -41,19 +86,23 @@ function App() {
   const onSubmit = () => {
     let previousValue = null;
     let newArr = [];
-    currentValue.forEach((value) => {
-      if (value != previousValue) {
-        newArr.push(value);
-        previousValue = value;
-      }
-    });
 
-    const joinedValue = newArr.join();
-    const formattedNumber = replaceComma(joinedValue);
-    const result = eval(formattedNumber);
+    if (currentValue.length > 0) {
+      currentValue.forEach((value) => {
+        if (value != previousValue) {
+          newArr.push(value);
+          previousValue = value;
+        }
+      });
 
-    setCurrentValue([]);
-    setCurrentResult(result);
+      const joinedValue = newArr.join();
+      const formattedNumber = replaceComma(joinedValue);
+      const result =
+        Math.round(1000000000000 * eval(formattedNumber)) / 1000000000000;
+
+      setCurrentValue([]);
+      setCurrentResult(result);
+    }
   };
 
   return (
